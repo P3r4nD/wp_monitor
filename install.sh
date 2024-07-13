763747c8b9093b94bbebbf341b1074cc2b6c2a6f
P3r4nD <git@parkteknia.com>
2024-07-13 23:56:42 +0200

    Update install.sh
    Error en en path jobs_file que se define en install.sh (Error en wpm_jobs.sh #28)

--- install.sh
+++ install.sh
@@ -168,7 +168,7 @@ update_wpm_jobs_file() {
     cp=$(detect_control_panel)
 
     # Update paths and panel in wpm_jobs.sh
-    sed -i "s|^JOBS_FILE=.*|JOBS_FILE=\"$BASE_DIR/wp_monitor/jobs\"|" "$WPM_JOBS_FILE"
+    sed -i "s|^JOBS_FILE=.*|JOBS_FILE=\"$BASE_DIR/jobs\"|" "$WPM_JOBS_FILE"
     sed -i "s|^JOBS_EXECUTED_FILE=.*|JOBS_EXECUTED_FILE=\"$BASE_DIR/jobs_executed\"|" "$WPM_JOBS_FILE"
     sed -i "s|^LOCK_FILE=.*|LOCK_FILE=\"$BASE_DIR/wpm_data/tmp/wp_monitor.lock\"|" "$WPM_JOBS_FILE"
     sed -i "s|^LOG_FILE=.*|LOG_FILE=\"$BASE_DIR/wpm_data/logs/jobs.log\"|" "$WPM_JOBS_FILE"
